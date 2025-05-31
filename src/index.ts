e7680c10162be11a97219ac2909a146f54b8325e
Vedant005 <vedantkanekar909@gmail.com>
2025-05-31 17:06:16 +0530

    fix: update dotenv import style and ensure fileRouter is included in the index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import { authenticateToken } from "./middleware/auth";
 import authRouter from "./routes/auth";
-
+import fileRouter from "./routes/file";
 dotenv.config({
   path: "./.env",
 });
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use("/auth", authRouter);
 
 app.use(authenticateToken as express.RequestHandler);
+app.use("/file", fileRouter);
 
 app.listen(8000, () => console.log("Server running on http://localhost:8000"));
