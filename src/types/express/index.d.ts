eb984d59dbf638c8a7db0dd88a123d0081e678ce
Vedant005 <vedantkanekar909@gmail.com>
2025-05-31 20:17:15 +0530

    feat: add custom types for Express request to include user and file properties

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,15 +1,18 @@
 import { Request } from "express";
 import * as multer from "multer";
 import { JwtPayload } from "jsonwebtoken";
-type User = {
-  id: number;
-  email: string;
-};
+import { User } from "@prisma/client";
+// type User = {
+//   id: number;
+//   email: string;
+//   password: string;
+// };
+import "express";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: JwtPayload & { id: number };
+      user?: { id: number };
       file?: Express.Multer.File;
     }
   }
