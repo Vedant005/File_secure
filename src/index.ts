55dbbc86ce4c1ea4d94d187346a526fb88181dc7
Vedant005 <vedantkanekar909@gmail.com>
2025-06-01 21:12:29 +0530

    refactor: reorganize comments for clarity in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import * as dotenv from "dotenv";
 import { authenticateToken } from "./middleware/auth";
 import authRouter from "./routes/auth";
 import fileRouter from "./routes/file";
+
 dotenv.config({
   path: "./.env",
 });
 
+// Initializing express app
 const app = express();
 app.use(express.json());
 
 app.use("/auth", authRouter);
 
 app.use(authenticateToken as express.RequestHandler);
+// routes below this as authenticated via the middleware
 app.use("/file", fileRouter);
 
 app.listen(8000, () => console.log("Server running on http://localhost:8000"));
