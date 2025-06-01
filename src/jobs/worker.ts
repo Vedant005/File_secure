c2d87788ff8d18df60ff51377c7f359df1809b05
Vedant005 <vedantkanekar909@gmail.com>
2025-06-01 21:50:20 +0530

    refactor: enhance documentation for BullMQ worker processing files

--- src/jobs/worker.ts
+++ src/jobs/worker.ts
@@ -4,9 +4,23 @@ import { prisma } from "../prisma";
 import * as fs from "fs";
 import * as crypto from "crypto";
 
-// Create a BullMQ worker that listens to the "fileQueue" queue
+/**
+ * BullMQ Worker to process files asynchronously
+ *
+ * Listens to the "fileQueue" queue and:
+ * - Fetches the file by ID from the database
+ * - Marks its status as "processing"
+ * - Reads the file content from disk
+ * - Generates a SHA-256 hash of the file
+ * - Stores the hash as `extracted_data` and updates status to "processed"
+ * - If an error occurs, sets the status to "failed"
+ *
+ * @param {object} job - Job data from the queue
+ * @param {number} job.data.fileId - ID of the file to be processed
+ */
 new Worker("fileQueue", async (job) => {
   const { fileId } = job.data;
+
   // Fetch the file record from the database using Prisma
   const file = await prisma.file.findUnique({ where: { id: fileId } });
 
